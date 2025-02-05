9ab1b1bfa17b68f51f9feccb5f87a77c76be7518
YAKOVE <yalhasov@gmail.com>
2025-02-05 08:25:27 +0200

    Project refactoring

--- conftest.py
+++ conftest.py
@@ -1,11 +1,15 @@
 import pytest
 from selenium import webdriver
 from time import sleep
+from selenium.webdriver.chrome.options import Options
 
 
 # yield allows to run the test and to return to the function
 @pytest.fixture()
 def browser():
+    options = Options()
+    Options.add_argument("--headless")
+    driver = webdriver.chrome(options=options)
     driver = webdriver.Chrome()
     driver.maximize_window()
     # allows Selenium to wait for the page to load to search for certain element
